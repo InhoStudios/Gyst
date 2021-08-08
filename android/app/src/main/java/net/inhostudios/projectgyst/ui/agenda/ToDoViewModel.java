6e564efe673402d0f67535e7f8bb1a7e8253cb98
andy <andyshenproper@gmail.com>
2021-08-07 19:48:29 -0700

    Add old android app and new flask directory

---
+++ android/app/src/main/java/net/inhostudios/projectgyst/ui/agenda/ToDoViewModel.java
@@ -0,0 +1,19 @@
+package net.inhostudios.projectgyst.ui.agenda;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ToDoViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ToDoViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
